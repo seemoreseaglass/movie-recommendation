f9cc3699a71803369de7ced5de626e9b21972ade
seemoreseaglass <haji.register@gmail.com>
2023-05-10 00:51:20 +0900

    debug: line60

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def login():
 
         # Query database for username
         rows = cur.execute("SELECT * FROM users WHERE username = ?", (username,))
-        print("fetchall(): ", rows.fetchall(), file=sys.stdout)
+        print("len(fetchall()): ", len(rows.fetchall()), file=sys.stdout)
         print("fetchone(): ", rows.fetchone(), file=sys.stdout)
         if len(rows.fetchall()) == 1:
             if check_password_hash(rows.fetchall()[0][2], (password,)):

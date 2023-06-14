fa43e48ea90b374279218160d0aebca6e7f1cacb
Hajime Ozawa <haji.register@gmail.com>
2023-06-14 15:36:47 +0900

    deleted "data"

--- static/scripts/main.mjs
+++ static/scripts/main.mjs
@@ -3,5 +3,5 @@ import { helpers } from './module.mjs';
 // Search
 let input = document.querySelector('.input_query');
 let activeRequest = null;
-let data = input.addEventListener('input', () => helpers.search(input, activeRequest));
+input.addEventListener('input', () => helpers.search(input, activeRequest));
 
